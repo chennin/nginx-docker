b041388bb5fe4e1f2c228f473535cba2720abc12
forbiddenlake <2653277+chennin@users.noreply.github.com>
2026-06-17 16:14:24 -0400

    Just use a date for a tag

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ nice buildah --storage-driver="$STORAGE_DRIVER" \
         -t "$CONT_LATEST" .
 
 buildah --storage-driver $STORAGE_DRIVER from --pull=never --name version-finder "$CONT_LATEST"
-REV=$(grep -oP '\d' argfile.conf | awk '{s+=$1} END {print s}')
+REV=$(date +"%Y%m%d")
 CONT_VER=$(buildah --storage-driver $STORAGE_DRIVER run version-finder sh -c "nginx -V 2>&1 | grep -oP '(?<=nginx version: nginx/)(.+)$'")-${REV}
 CONT_VER=${CONT_VER//[+~]/_}
 echo "Container version: ${CONT_VER}"
